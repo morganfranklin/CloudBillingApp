994d87c9811b3ac472ddbf3953e4e284afed312a
Tomasz Zarzycki <tomasz.zarzycki@morganfranklin.com>
2018-01-28 21:57:05 -0500

    initial reconfig of authentication / role determination routine from PS
    Signed-off-by: Tomasz Zarzycki <tomasz.zarzycki@morganfranklin.com>

--- ViewController/src/view/PeopleSoftCallSecurityFilter.java
+++ ViewController/src/view/PeopleSoftCallSecurityFilter.java
@@ -182,7 +182,7 @@ public class PeopleSoftCallSecurityFilter {
         }
         */
         // test override
-        this.setRetrievedToken("admin");
+        this.setRetrievedToken("gbewley");
 
         this.checkRoles(this.getRetrievedToken());
 
